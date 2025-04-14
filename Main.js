c07e870361d0dcfc5cf41c3c5cc49eae7ccf2771
Brooklyn007 <iloegbunamebube2006@gmail.com>
2025-04-14 10:58:36 +0100

    iphone calculator

---
+++ Main.js
@@ -0,0 +1,74 @@
+
+// const title = document.getElementById('main-heading');
+// console.log(title);
+// const title = document.querySelector('#main-heading');
+// title.style.color = 'red';
+// console.log(title);
+// let x = 5;
+// console.log(x);
+
+function global() {
+    let x = 50;
+    console.log(x);
+
+}
+
+global()
+let bassey = "mumu";
+console.log(bassey);
+
+function ageFunction() {
+    let age = 18;
+    console.log(age);
+
+}
+ageFunction()
+
+function nameFunction() {
+    let name = "brooklyn";
+    return name;
+}
+
+console.log(nameFunction());
+
+let x = "alice" * "blossom"
+// console.log(x);
+
+let z = 2 === "2"
+
+// console.log(z);
+
+function comparison() {
+    let num1 = 10
+    let num2 = 25
+    let sum = num1 > num2
+        sum ? "true" : "false"
+    return sum
+}
+
+// console.log(comparison());
+
+function summation() {
+    let x = 3
+    let y = 5
+    if (x > y) {
+        return yes
+    } else{
+        return false
+    }
+}
+console.log(summation());
+
+function dateTime() {
+    const now = new Date()
+    return now
+}
+
+console.log(dateTime());
+// alert("hello world");
+// console.error("noo");   
+let a = 45
+console.log(typeof(a));
+let m = 16 + "david"
+console.log(m);
+
